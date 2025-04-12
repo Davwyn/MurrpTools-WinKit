8908e358ecd1885d4c2fb0faa88300b710d8c398
Davwyn <dafydddragon@gmail.com>
2025-04-11 21:18:15 -0500

    Fixed Harvest_Drivers.cmd

--- MurrpTools/MediaFiles/Harvest_Drivers.cmd
+++ MurrpTools/MediaFiles/Harvest_Drivers.cmd
@@ -1,9 +1,7 @@
-powershell.exe -ExecutionPolicy Bypass -File %~dp0Harvest_Drivers.ps1 -NoExit
-
 @echo off
 if not exist "%~dp0Harvest_Drivers.ps1" (
   echo Error: 1 Harvest_Drivers.ps1 not found.
   pause
   exit /b 1
 )
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0Harvest_Drivers.ps1"
+powershell.exe -NoExit -ExecutionPolicy Bypass -File "%~dp0Harvest_Drivers.ps1"
